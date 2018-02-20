2af3c9eea801cde9dba5df3806a65068e03f6225
Murugan Viswanathan <murugan.viswanathan@gmail.com>
2018-02-19 17:57:30 -0800

    wip:add redis client

---
+++ FindPackageHiredis.cmake
@@ -0,0 +1,17 @@
+# Find hiredis opensourse package include & library path 
+#
+FIND_PATH(HIREDIS_INCLUDE 
+	hiredis.h 
+	/usr/local/include/hiredis/library)
+	
+FIND_LIBRARY(HIREDIS_LIBRARY 
+	NAMES hiredis 
+	PATHS /usr/lib /usr/local/lib )
+
+INCLUDE(FindPackageHandleStandardArgs)
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(HIREDIS DEFAULT_MSG 
+	HIREDIS_INCLUDE 
+	HIREDIS_LIBRARY)
+
+MARK_AS_ADVANCED(HIREDIS_LIBRARY HIREDIS_INCLUDE)
